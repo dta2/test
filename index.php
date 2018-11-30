f11f69115568c1d80a64327a141a12dcff786f73
Elias <dta2@protonmail.com>
2018-11-30 14:43:56 +0100

    Ajout de l'image du thermomètre et du fichier data.txt

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
 </head>
 <body>
+
+	<h1>Température</h1>
+
+	<br><br>
 
 	<?php 
 		echo "ça fonctionne";
